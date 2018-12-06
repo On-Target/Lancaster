cbf5507ec09cbc715d304cd4fda59f5dcc6fb8da
Brandon Spaulding <brandonjspaulding@gmail.com>
2018-12-06 02:46:17 -0600

    v1
    new layout for LCH LP

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.custom1').owlCarousel({
+        animateOut: 'fadeOut',
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        items:1,
+        loop:true,
+        smartSpeed:450
+    });
+
+
+$('#details-btn').on('click',function(){
+
+    //$(".caption-text").css('display','block');
+    $(".caption-text").toggle();
+
+    $(this).text(function(i, text){
+        return text === "Hide Details" ? "Show Details" : "Hide Details";
+    })
+
+
+
+
+});
+
+
+// $( "#target" ).click(function() {
+//     alert( "Handler for .click() called." );
+//   });
+
+  });
+
